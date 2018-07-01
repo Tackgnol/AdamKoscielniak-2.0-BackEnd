1fada0936acd0935fe1974655032bb5ed242654d
Tackgnol <koscielniakadam@gmail.com>
2018-07-01 21:06:25 +0200

    Added a expirience query endpoint

--- Controllers/Experience.py
+++ Controllers/Experience.py
@@ -1,16 +1,18 @@
 from main import app
 from Models.Experience import Experience
 from GlobalAPi.Result import Result
-from flask import request
+from flask import request, Response
 import json
+from datetime import datetime
 import mongoengine
 from mongoengine.errors import InvalidQueryError
+
 
 @app.route('/experience/add', methods=['POST'])
 def AddExperience():
     result = Result()
-    experience = request.get_json(force = True)
-    experienceJSON = json.dumps(experience) 
+    experience = request.get_json(force=True)
+    experienceJSON = json.dumps(experience)
     try:
         job = Experience.from_json(experienceJSON)
         job.save()
@@ -23,6 +25,7 @@ def AddExperience():
 
     return result.ToResponse()
 
+
 @app.route('/experience/<id>', methods=['GET'])
 def GetExperienceById(id):
     result = Result()
@@ -34,36 +37,75 @@ def GetExperienceById(id):
     except Exception:
         result.AddError('Unknown error consult the system administrator')
 
-    return result.ToResponse()
-    
+    resp = Response(result.ToResponse())
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+
+    return resp
+
+
 @app.route('/experience/<id>', methods=['PUT'])
 def UpdateExperienceById(id):
     result = Result()
-    update = request.get_json(force = True)
+    update = request.get_json(force=True)
     try:
-        dbObj = Experience.objects.filter(Id = id).first()
+        dbObj = Experience.objects.filter(Id=id).first()
         updateDbObj = dbObj.to_mongo()
-        for (key,value) in update.items():
+        for (key, value) in update.items():
             updateDbObj[key] = value
-        
+
         del updateDbObj['_id']
         dbObj.update(**updateDbObj)
         result.Value = dbObj.to_json()
     except AttributeError:
-        result.AddError('This experience does not exist, perhaps you wished to add it?')
-    except InvalidQueryError: 
+        result.AddError(
+            'This experience does not exist, perhaps you wished to add it?')
+    except InvalidQueryError:
         result.AddError('Invalid field in the update statement, please review')
     except Exception:
-        result.AddError('Unknown error consult the system administrator')  
+        result.AddError('Unknown error consult the system administrator')
     return result.ToResponse()
+
+
+@app.route('/experience', methods=['GET'])
+def GetExperiences():
+    result = Result()
+    expFrom = request.args.get('from') if request.args.get(
+        'from') is not None else '1988-01-01'
+    expTo = request.args.get('to') if request.args.get(
+        'to') is not None else '3000-01-01'
+    expSkills = request.args.get('skills').split(',') if request.args.get(
+        'skills') is not None else []
+
+    try:
+        parsedDateFrom = datetime.strptime(expFrom, '%Y-%m-%d')
+        parsedDateTo = datetime.strptime(expTo, '%Y-%m-%d')
+
+    except ValueError:
+        result.AddError('Invalid Datetime format')
+
+    if parsedDateFrom > parsedDateTo:
+        result.AddError('Date from cannot be greater then Date from')
+        return result.ToResponse()
+
+    query_experience = Experience.objects(
+        BeginDate__gte=parsedDateFrom, EndDate__lte=parsedDateTo)
+
+    if len(expSkills) > 0:
+        query_experience = query_experience.filter(Skills__all=expSkills)
+
+    result.Value = query_experience.to_json()
+
+    return result.ToResponse()
+
+
 @app.route('/experience/<id>', methods=['DELETE'])
 def DeleteExperieceById(id):
     result = Result()
-    try: 
-        toDelete = Experience.objects.filter(Id = id).first()
+    try:
+        toDelete = Experience.objects.filter(Id=id).first()
         toDelete.delete()
         result.Value = "Element Removed succesfully"
     except AttributeError:
-        result.AddError('This experience does not exist, perhaps it was already deleted?')
+        result.AddError(
+            'This experience does not exist, perhaps it was already deleted?')
     return result.ToResponse()
-
--- GlobalAPi/Result.py
+++ GlobalAPi/Result.py
@@ -1,39 +1,31 @@
 import json
 from .Exceptions import BadRequest
 
+
 class Result:
     def __init__(self):
         self.Errors = []
         self.Warnings = []
-        self.Infos = []
         self.Value = None
-        self.IsError = False,
-        self.IsWarning = False,
-        self.IsInfo = False
 
     def AddError(self, message):
         self.Errors.append(message)
         self.IsError = True
-    
+
     def AddWarning(self, message):
         self.Warnings.append(self, message)
         self.IsWarning = True
-    
-    def AddInfo(self, message): 
+
+    def AddInfo(self, message):
         self.Infos.append(self, message)
         self.IsInfo = True
 
     def ToResponse(self):
         responseDict = {
-            'IsError': self.IsError,
-            'IsWarning': self.IsWarning,
-            'IsInfo': self.IsInfo,
-            'Errors' : self.Errors,
-            'Warnings' : self.Warnings,
-            'Infos': self.Infos,
+            'Errors': self.Errors,
+            'Warnings': self.Warnings,
             'Value': self.Value,
         }
         if len(self.Errors) > 0:
             raise(BadRequest("There are errors in the query", payload=responseDict))
         return json.dumps(responseDict)
-
